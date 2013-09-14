8b32c5cb882c1c21d0daae5c546e038b25c23505
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-09-14 23:48:01 +0400

    Произвожу парсинг sln файла, получая относительные пути проектов

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,5 +1,4 @@
 mdtool = "/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool"
-sln_path = "/Users/rzaitov/Documents/Apps/BuildScript/BuildSample/BuildSample/CoolApp.csproj"
-all_projects = ["MacWarehouse", "Counts", "Sales"]
+sln_path = "/Users/rzaitov/Documents/Apps/BuildScript/BuildSample/BuildSample.sln"
 
-projects_to_exclude = ["Counts"]
+projects_to_exclude = ["NotCompileApp"]
