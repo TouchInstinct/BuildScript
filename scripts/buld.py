c4631e1805d743a9bbf9e66173490747b697077d
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-09-15 18:21:40 +0400

    Реализовал патчинг файлов для ios (sln info.plist)

--- scripts/buld.py
+++ scripts/buld.py
@@ -3,32 +3,25 @@ import os
 import settings
 import instruments
 
-# print("all projects:")
-# print(settings.all_projects)
-
-# project_to_remove = settings.projects_to_exclude[0];
-# print("project to remove:")
-# print(project_to_remove);
-
-
-# projects_to_build = settings.all_projects[:]
-# projects_to_build.remove(project_to_remove)
-
-# print("projects to build:")
-# print(projects_to_build)
-
-cmd_args = {'version' : '1.2.3', 'some_field' : 'some_value'}
-# sln_dir = os.path.dirname(settings.sln_path)
-
-# instruments.CreateOrRestoreFromBackup(sln_dir, settings.files_for_backup)
-# instruments.RemoveProjectFromSolution(settings.sln_path, settings.projects_to_exclude)
-# instruments.CleanSolution(settings.mdtool, settings.sln_path)
-# instruments.BuildSolution(settings.mdtool, settings.sln_path, sln_config)
+cmd_args = {'version' : '1.2.3', 'some_field' : 'some_value', 'action' : 'setup'}
 
 instruments.CompileConfigs(settings.build_ready_configs, cmd_args)
 
-for conf in settings.build_ready_configs:
-	print conf
+for bc in settings.build_ready_configs:
+	sln_path = bc['sln_path']
+	sln_dir = os.path.dirname(sln_path)
 
+	instruments.CreateOrRestoreFromBackup(sln_dir, bc['files_for_backup'])
+	instruments.RemoveProjectFromSolution(sln_path, bc['projects_to_exclude'])
 
-# instruments.DeleteBackups(sln_dir, settings.files_for_backup)
+	# try patch source code files
+	path_function = bc['patch']
+	if path_function is not None:
+		path_function(bc)
+
+	if bc['action'] == 'build':
+		instruments.CleanSolution(bc['mdtool'], sln_path)
+		instruments.BuildSolution(bc['mdtool'], sln_path, bc['sln_config'])
+
+		instruments.CreateOrRestoreFromBackup(sln_dir, settings.files_for_backup)
+		instruments.DeleteBackups(sln_dir, settings.files_for_backup)
--- scripts/patch.py
+++ scripts/patch.py
@@ -1,6 +1,43 @@
-def PathcIos(build_config):
-	print('start patch ios')
+import re
+import os
 
+def RewriteFile(file_to_rewrite, content):
+	file_to_rewrite.seek(0)
+	file_to_rewrite.write(content)
+	file_to_rewrite.truncate()
+	file_to_rewrite.close()
+
+
+def PatchSlnForIos(build_config):
+
+	sln_file = open(build_config['sln_path'], 'r+')
+	content = sln_file.read()
+
+	condesign_key_patt = r'<CodesignKey>.*?</CodesignKey>'
+	condesign_key_node = r'<CodesignKey>{0}</CodesignKey>'.format(build_config['codesign_key'])
+	content = re.sub(condesign_key_patt, condesign_key_node, content)
+
+	RewriteFile(sln_file, content)
+
+def PatchInfoPlist(build_config):
+
+	sln_dir = os.path.dirname(build_config['sln_path'])
+	abs_info_plist_path = os.path.join(sln_dir, build_config['info_plist_rel_path'])
+
+	info_plist_file = open(abs_info_plist_path, 'r+')
+	content = info_plist_file.read()
+
+	version_patt = r'\s*<key>CFBundleVersion</key>\s+<string>[\d\.]+</string>'
+	version_node = r'\n\t<key>CFBundleVersion</key>\n\t<string>{0}</string>'.format(build_config['version'])
+	content = re.sub(version_patt, version_node, content)
+
+	RewriteFile(info_plist_file, content)
+
+def PathcIos(build_config):
+
+	PatchSlnForIos(build_config)
+	PatchInfoPlist(build_config)
 
 def PathcAndroid(build_config):
 	print('start patch ios')
+
