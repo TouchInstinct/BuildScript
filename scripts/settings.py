3c164f73b2decb265c564135d604f72378eb482c
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-09-29 19:27:23 +0400

    удалил лишние пробелы перед символом ':'

--- scripts/settings.py
+++ scripts/settings.py
@@ -2,10 +2,10 @@
 import patch
 
 build_root = {
-	'mdtool' : '/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool',
-	'version' : '0.0.0',
-	'action' : 'build', # 'build'|'setup'
-	'parent' : None
+	'mdtool': '/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool',
+	'version': '0.0.0',
+	'action': 'build', # 'build'|'setup'
+	'parent': None
 }
 
 ios_root = {
@@ -14,31 +14,31 @@ ios_root = {
 }
 
 ios_development_root = {
-	'files_for_backup' : ['BuildSample.sln', 'BuildSample/CoolApp.csproj', 'BuildSample/Info.plist'],
-	'projects_to_exclude' : ['NotCompileApp'],
-	'info_plist_rel_path' : 'BuildSample/Info.plist',
-	'patch' : patch.PathcIos,
-	'parent' : ios_root
+	'files_for_backup': ['BuildSample.sln', 'BuildSample/CoolApp.csproj', 'BuildSample/Info.plist'],
+	'projects_to_exclude': ['NotCompileApp'],
+	'info_plist_rel_path': 'BuildSample/Info.plist',
+	'patch': patch.PathcIos,
+	'parent': ios_root
 }
 
 ios_development_production = {
-	'name' : 'ios_development_production',
+	'name': 'ios_development_production',
 
-	'sln_config' : 'Release|iPhone',
-	'codesign_key' : 'iPhone Developer: Рустам Заитов (CTL85FZX6K)',
-	'codesign_provision' : '8F606DAE-F9C9-4A19-8EFF-34B990D76C28',
+	'sln_config': 'Release|iPhone',
+	'codesign_key': 'iPhone Developer: Рустам Заитов (CTL85FZX6K)',
+	'codesign_provision': '8F606DAE-F9C9-4A19-8EFF-34B990D76C28',
 
 	'parent' : ios_development_root
 }
 
 ios_development_staging = {
-	'name' : 'ios_development_staging',
+	'name': 'ios_development_staging',
 
-	'sln_config' : 'Debug|iPhone',
-	'codesign_key' : 'iPhone Developer',
-	'codesign_provision' : 'F82B1481-F3D0-4CB5-AA6E-8B8D8E3A9DC1',
+	'sln_config': 'Debug|iPhone',
+	'codesign_key': 'iPhone Developer',
+	'codesign_provision': 'F82B1481-F3D0-4CB5-AA6E-8B8D8E3A9DC1',
 
-	'parent' : ios_development_root
+	'parent': ios_development_root
 }
 
 ios_app_store = {
