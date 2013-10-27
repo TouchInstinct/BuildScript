b8b600c4a2f7d0b10e5d5f1ea3d2a51432e91dd7
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-10-28 03:22:19 +0400

    Реализовал копирование профиля обеспечения с помощью команды копирования

--- scripts/ManualTests/copy_test.py
+++ scripts/ManualTests/copy_test.py
@@ -2,15 +2,15 @@ from CommandBuilders.CopyCommandBuilder import CopyCommandBuilder
 from ManualTests.path_provider import PathProvider
 
 line1 = "copy 'BuildSample/BuildSample.sln' to 'BuildSample/BuildSample.txt'"
-line2 = "copy 'BuildSample/BuildSample.sln' to '~/Downloads/BuildSample.sln'"
+line2 = "copy 'BuildSample/BuildSample/Profiles/8F606DAE-F9C9-4A19-8EFF-34B990D76C28.mobileprovision' to '~/Library/MobileDevice/Provisioning Profiles/BuildScript.mobileprovision'"
 
 baseDir = '../'
 path_provider = PathProvider(baseDir)
 
 copyCmdBuilder = CopyCommandBuilder(path_provider)
 
-copyCmdToRel = copyCmdBuilder.getCommandFor(line1)
-copyCmdToRel.execute()
+#copyCmdToRel = copyCmdBuilder.getCommandFor(line1)
+#copyCmdToRel.execute()
 
 copyCmdToAbs = copyCmdBuilder.getCommandFor(line2)
 copyCmdToAbs.execute()
