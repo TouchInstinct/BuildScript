00832523b753a1e2a1dc7a03e694382d86aeaf06
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-10-28 02:24:28 +0400

    добавил возможность получения путей к файлам в кавычках если они сожержат пробелы. Но видимо это бесполезное улучшение

--- scripts/parser/CopyParser/CopyArguments.py
+++ scripts/parser/CopyParser/CopyArguments.py
@@ -15,3 +15,17 @@ class CopyArguments():
 		result &= self.target is not None
 
 		return  result
+
+	def getSafeSource(self):
+		safeSource = self.__makeSafe(self.source)
+		return safeSource
+
+	def getSaveTarget(self):
+		safeTarget = self.__makeSafe(self.target)
+		return safeTarget
+
+	def __makeSafe(self, filePath):
+		assert filePath is not None
+
+		safe = filePath if ' ' not in filePath else '"{0}"'.format(filePath)
+		return safe
