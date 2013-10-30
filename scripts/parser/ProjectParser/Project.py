33c9f97e1e7e2bbeeaedc4583b2d69a41d154a46
rzaitov <r-zaitov@yandex.ru>
2013-10-30 19:34:27 +0400

    избавился от ненужных файлов

--- scripts/parser/ProjectParser/Project.py
+++
@@ -1,7 +0,0 @@
-class Project:
-	def __init__(self, projectName):
-		self.projectName = projectName
-		self.settings = {}
-
-	def __str__(self):
-		return 'app Name: {0} settings: {1}'.format(self.projectName, self.settings)
