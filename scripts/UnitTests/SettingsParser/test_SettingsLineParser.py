515b6fba1fad784afa6cae1abcdd13a380ebcfd4
rzaitov <r-zaitov@yandex.ru>
2013-10-31 16:15:19 +0400

    написал тест к методу разделения входной строки на значение настроки и строку с сегментами

---
+++ scripts/UnitTests/SettingsParser/test_SettingsLineParser.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import unittest
+from parser.SettingsParser.SettingsLineParser import SettingsLineParser
+
+
+class TestSettingsLineParser(unittest.TestCase):
+	def setUp(self):
+		self.parser = SettingsLineParser()
+
+	def test_getPathAndValue(self):
+		line = "abc.123.some_name = 'crazy value ±~ ../ 123'"
+		result = self.parser.splitToPathAndValue(line)
+
+		self.assertEqual('abc.123.some_name', result[0])
+		self.assertEqual('crazy value ±~ ../ 123', result[1])
