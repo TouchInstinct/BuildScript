5ce8eb0269791293a0c78200bab4d81822de35e4
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-11-05 01:02:44 +0400

    Добавил класс, отвечающий за удаление комментариев из входной строки

--- scripts/Core/LineConveyor/CommentRemover.py
+++ scripts/Core/LineConveyor/CommentRemover.py
@@ -2,5 +2,9 @@ class CommentRemover:
 	def processLine(self, line):
 		assert line is not None
 
+		newLine = line
+		index = line.find('#')
+		if index >= 0:
+			newLine = line[:index]
 
-
+		return newLine
