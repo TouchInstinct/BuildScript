2ff6cce5555d5679164f2d9e456ea0820ddda7f5
rzaitov <r-zaitov@yandex.ru>
2013-11-07 18:48:12 +0400

    потестил возможность определения post install

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,6 +1,19 @@
-import os
-import site
+from distutils.command.install import install
 from distutils.core import setup
+
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument('--c', required=False)
+#parser.add_argument('--install-headers', required=False)
+#args = parser.parse_known_args()[0]
+#print args
+#print parser.parse_known_args()[1]
+#print args.c
+#import sys
+#print sys.argv
+#if args.c == 'install':
+#	path = getattr(args, 'install-headers', None)
+#	print path
 
 setup(name= 'TouchinBuild',
 	version= '0.0.10',
@@ -16,14 +29,13 @@ setup(name= 'TouchinBuild',
 	author = 'Rustam Zaitov',
 	author_email = 'rustam.zaitov [at] touchin.ru',
 	maintainer= 'Rustam Zaitov',
-	maintainer_email='rustam.zaitov [at] touchin.ru'
+	maintainer_email='rustam.zaitov [at] touchin.ru',
 )
 
 
-scriptFilePath = os.path.abspath(__file__)
-
-sites = site.getsitepackages()
-print sites
+#scriptFilePath = os.path.abspath(__file__)
+#sites = site.getsitepackages()
+#print sites
 
 #scriptDirPath = os.path.dirname(scriptFilePath)
 #absPathToEnterPoint = os.path.join(scriptDirPath, 'TouchinBuild')
